4b1a536be45c4cd8811cbee0a1aa2c676fe94cac
tovehydmark <tovehydmark@gmail.com>
2021-12-01 19:08:43 +0100

    clarified describing texts

--- main.js
+++ main.js
@@ -1,76 +1,68 @@
-//Kalla på det vi behöver
+//ELEMENTS NEEDED FOR HEADER
 const header = document.getElementById("header");
-const main = document.getElementById("main");
-let myParagraph = document.getElementsByClassName("pageParagraph")[0];
+//USER NAME INPUT
+const userName = document.createElement("input");
+userName.setAttribute("id", "userName");
+userName.setAttribute("type", "text")
 
+//PASSWORD INPUT
+const userPassword = document.createElement("input")
+userPassword.setAttribute("id", "userPassword");
+userPassword.setAttribute("type", "password")
+
+//LOGOUT BUTTON
+const logoutBtn = document.createElement("button");
+
+
+//ELEMENTS NEEDED FOR MAIN
+const main = document.getElementById("main");
+//PARAGRAPH WHIC I CAN CHANGE CONTENT OF
+let myParagraph = document.getElementsByClassName("pageParagraph")[0];
 
 
 //HEADER
 //Utloggad: visa ett inloggningsformulär
 function showLoginForm() {
-    const userName = document.createElement("input");
     header.append(userName);
-    userName.setAttribute("id", "userName");
-    userName.setAttribute("type", "text")
-
-    const userPassword = document.createElement("input")
     header.append(userPassword);
-    userPassword.setAttribute("id", "userPassword");
-    userPassword.setAttribute("type", "password")
 }
-//showLoginForm();
+showLoginForm();
+
 
 //Inloggad: visa logga-ut knapp
 function showLogoutBtn() {
-    const logoutBtn = document.createElement("button");
     header.append(logoutBtn);
     logoutBtn.innerText = "log out";
 }
 //showLogoutBtn()
-
-
 
 //3 STATES FOR MAIN:
 //State för ej inloggade
 function notLoggedIn() {
     myParagraph.innerHTML = "You are not logged in.";
 }
+notLoggedIn()
 
 //State för inloggad
-
 function loggedIn() {
     myParagraph.innerHTML = "Welcome Janne, you are logged in! This is good news for me because that means the login function is working.";
 }
+//loggedIn()
 
 //State för fel lösenord
 function wrongDetails() {
     myParagraph.innerHTML = "You seem to have entered the wrong login details, please try again.";
 }
+//wrongDetails()
 
 
 
-//MAIN
-//Skapa rätt sidvy med Switch
 
 
 
-//Skapa funktioner för varje vy och kalla på dem i if-else i headern? 
-
-// function ickeInloggad () = {
-//    paragraph.innerHTML = "You are not logged in";
-// }
-
-//State för inloggad
-
-// function inloggad () = {
-//    
-// }
 
 
-//State för fel lösen
-// function felLösen () = {
-//    
-// }
+
 
 
 
